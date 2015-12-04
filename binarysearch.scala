b073b99de910cbdf9b1e0c258aa1ccd065dca500
Lucas de Castro Lima Matos <lucas.itapas@hotmail.com>
2015-12-03 23:22:40 -0300

    Finish interactive binary search

--- binarysearch.scala
+++ binarysearch.scala
@@ -1,33 +1,41 @@
 class BinarySearch(list: Array[Int]) {
-    def find(x: Int, len: Int): Array[Int] = {
-        var aux = len /2
+
+    def find(x: Int, len: Int, count: Int): Int = {
+        var aux  = len
+        var plus = count
         var elem: Int = list(aux)
 
         if(elem == x){
-            return Array(x, aux)
-        }else if(elem > x){
-            aux = aux + (aux / 2)
-            find(x, aux)
-        }else{
-            aux = aux - (aux / 2)
-            find(x, aux);
+            println("Valor: "+ elem +" Key: "+ aux);
+            return 0;
+        }else if(x > elem){
+            var vl = Math.round(aux - (aux / 2))
+            if(vl > 1)
+                vl /= 2
+            aux += vl
+        }else if(x < elem){
+            var vl = Math.round(aux - (aux / 2))
+            if(vl > 1)
+                vl /= 2
+            aux -= vl
         }
+
+        if(count > list.length){
+            println("Resultado não encontrado")
+            return 1;
+        }else{
+            plus = count + 1
+            find(x, aux, plus);
+        }
+
     }
 
     def getLenght(): Int = {
-        return list.length - 1;
+        return (list.length - 1) / 2;
     }
 
-    def printArr(ar: Array[Int]){
-        print("Valor: ")
-        print(ar(0))
-        print(" Key: ")
-        print(ar(1))
-        println()
-    }
 }
 
 var list = Array(2,9,20,30,56,556,565)
 var binary = new BinarySearch(list)
-var result = binary.find(30, binary.getLenght)
-binary.printArr(result)
+var result = binary.find(93, binary.getLenght, 0)
---
+++ binarysearch_interactive.scala
@@ -0,0 +1,42 @@
+class BinarySearch(list: Array[Int]) {
+
+    def find(x: Int) = {
+        var aux = (list.length - 1) /2
+        var mit = true;
+        var res = 0;
+        var count = 0;
+        while(mit){
+            var elem: Int = list(aux)
+            println(aux+" "+elem);
+            if(elem == x){
+                res = elem;
+                mit = false;
+            }else if(x > elem){
+                var vl = Math.round(aux - (aux / 2))
+                if(vl > 1)
+                    vl /= 2
+
+                aux += vl
+            }else if(x < elem){
+                var vl = Math.round(aux - (aux / 2))
+                if(vl > 1)
+                    vl /= 2
+
+                aux -= vl
+            }
+
+            if(count > list.length){
+                println("Resultado não encontrado")
+                mit = false;
+            }
+
+            count += 1
+        }
+        if(!(count > list.length))
+            println("Valor: "+ res +" Key: "+ aux);
+    }
+}
+
+var list = Array(2,9,20,30,56,556,565)
+var binary = new BinarySearch(list)
+binary.find(23)
